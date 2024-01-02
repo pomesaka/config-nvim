a0fe67ea294d98adfaaac68437a176297065682b
pomesaka <tksk4yh@yahoo.co.jp>
2024-01-02 18:39:43 +0900

    chore git and telescope keymaps

--- lua/plugin/gitsigns.lua
+++ lua/plugin/gitsigns.lua
@@ -13,18 +13,18 @@ return {
   },
   keys = {
     -- Navigation
-    {']g', '<cmd>Gitsigns next_hunk<CR>'},
-    {'[g', '<cmd>Gitsigns prev_hunk<CR>'},
+    { ']g',         '<cmd>Gitsigns next_hunk<CR>' },
+    { '[g',         '<cmd>Gitsigns prev_hunk<CR>' },
 
     -- Actions
-    {'<leader>ga', '<cmd>Gitsigns stage_hunk<CR>', mode = {'n', 'v'}},
-    {'<leader>gr', '<cmd>Gitsigns reset_hunk<CR>', mode = {'n', 'v'}},
-    {'<leader>gA', '<cmd>Gitsigns stage_buffer<CR>'},
-    {'<leader>gu', '<cmd>Gitsigns undo_stage_hunk<CR>'},
-    {'<leader>gR', '<cmd>Gitsigns reset_buffer<CR>'},
-    {'<leader>gp', '<cmd>Gitsigns preview_hunk<CR>'},
-    {'<leader>gb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>'},
-    {'<leader>gd', '<cmd>Gitsigns diffthis<CR>'},
-    {'<leader>gD', '<cmd>lua require"gitsigns".diffthis("~")<CR>'},
+    -- {'<leader>ga', '<cmd>Gitsigns stage_hunk<CR>', mode = {'n', 'v'}},
+    -- {'<leader>gr', '<cmd>Gitsigns reset_hunk<CR>', mode = {'n', 'v'}},
+    -- {'<leader>gA', '<cmd>Gitsigns stage_buffer<CR>'},
+    -- {'<leader>gu', '<cmd>Gitsigns undo_stage_hunk<CR>'},
+    -- {'<leader>gR', '<cmd>Gitsigns reset_buffer<CR>'},
+    { '<leader>gp', '<cmd>Gitsigns preview_hunk<CR>' },
+    { '<leader>gb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>' },
+    { '<leader>gd', '<cmd>Gitsigns diffthis<CR>' },
+    { '<leader>gD', '<cmd>lua require"gitsigns".diffthis("~")<CR>' },
   },
 }
