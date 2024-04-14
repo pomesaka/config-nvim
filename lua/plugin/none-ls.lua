ce2f993443c5877460515002f09a3742304d2e6d
pomesaka <takasaka_kazuki@accelhack.com>
2024-04-14 09:08:27 +0900

    update biome settings

--- lua/plugin/none-ls.lua
+++ lua/plugin/none-ls.lua
@@ -3,10 +3,12 @@ return {
   opts = function(_, opts)
     local nls = require("null-ls").builtins
     opts.sources = vim.list_extend(opts.sources or {}, {
+      -- nls.formatting.biome,
       nls.formatting.biome.with({
         args = {
           'check',
           '--apply-unsafe',
+          '--indent-style=space',
           '--formatter-enabled=true',
           '--organize-imports-enabled=true',
           '--skip-errors',
